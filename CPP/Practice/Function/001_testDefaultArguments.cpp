a4fa8eb34f8044c8c6255fd11c166a80325e5f86
shreechandansamal <shreechandansamal@gmail.com>
2026-06-20 11:49:15 +0530

    Function: added 001_testDefaultArguments.cpp

---
+++ CPP/Practice/Function/001_testDefaultArguments.cpp
@@ -0,0 +1,30 @@
+/**
+ * Task:
+ *  test how default arguments works in cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int add(int, int, int=0); //assigned 0 to the 3rd variable 
+
+int main()
+{
+    int a, b, c;
+
+    cout<<"Enter two numbers ";
+    cin>>a>>b;
+    cout<<"Sum is "<<add(a, b)<<endl;
+
+    cout<<"Enter three numbers ";
+    cin>>a>>b>>c;
+    cout<<"Sum is "<<add(a, b, c)<<endl;   
+
+    return 0;
+}
+
+int add(int x, int y, int z)
+{
+    return x + y + z;
+}
