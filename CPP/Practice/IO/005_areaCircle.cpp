4177f9f210d43059152b8e9895a9c070b3fd08e0
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 11:29:48 +0530

    IO: added 005_areaCircle.cpp

---
+++ CPP/Practice/IO/005_areaCircle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    const float pi = 3.1416f;
+    cout<<"Enter radius of circle: ";
+    cin>>r;
+    cout<<"Area of circle is "<<(r * r) * pi<<endl;
+}
