bd8df81c5bf0ebd832d8a139696246efdbc67b16
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 11:54:16 +0530

    IO: added 007_averageOfThreeNum.cpp

---
+++ CPP/Practice/IO/007_averageOfThreeNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+    int totalNum = 3;
+    
+    cout<<"Enter 3 Numbers for average: ";
+    cin>>num1>>num2>>num3;
+    cout<<"Average of 3 numbers is "<< (num1 + num2 + num3) / totalNum <<endl;
+
+    return 0;
+}
