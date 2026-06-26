69bd8c0b8895e564da24b4ff222781d7fd7dadac
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 11:38:26 +0530

    IO: added 006_volumeCuboid.cpp

---
+++ CPP/Practice/IO/006_volumeCuboid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float l;
+    float w;
+    float h;
+    cout<<"Enter Length, Width, Height of a cuboid: ";
+    cin>>l>>w>>h;
+    cout<<"Volume of cuboid is "<< l * w * h <<endl;
+}
