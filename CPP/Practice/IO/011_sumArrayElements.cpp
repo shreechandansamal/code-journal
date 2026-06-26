9220273834e657a8b68a054f7c51ea2a56a21e9b
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 22:25:21 +0530

    IO: updated logic in 011_sumArrayElements.cpp

--- CPP/Practice/IO/011_sumArrayElements.cpp
+++ CPP/Practice/IO/011_sumArrayElements.cpp
@@ -1,6 +1,14 @@
 /**
  * Task: 
  *  Write a C++ program to add all the numbers of an array of size 10.
+ * 
+ * Update:
+ *  in a single for loop cin and sum is actually not meaningful, 
+ *  because in that case we are actually not using the array for
+ *  the sum thats why by twice the for loop do the correct thing,
+ *  first for loop will store the 10 numbers into array, next for
+ *  loop will retrive the values one by one and add with each other
+ *  to find the total value. 
  */
 
 #include <iostream>
@@ -14,10 +22,17 @@ int main()
     int Total = 0;
     
     cout<<"Enter Ten Numbers for Sum: "<<endl;
+
+    //store 10 numbers into array
     for (int i = 0; i < count; i++)
     {
         cin>>num[i];
         // cout<<"DEBUG: input number is "<<num[i]<<endl;
+    }
+
+    //retrive those 10 number value and add one by one
+    for (int i = 0; i < count; i++)
+    {
         Total += num[i]; 
         // cout<<"DEBUG: current Total is "<<Total<<endl;
     }
