574bc0938ea205e87f33fa0ba07cf78a9c348699
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 12:00:25 +0530

    IO: added 008_squareNum.cpp

---
+++ CPP/Practice/IO/008_squareNum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    
+    cout<<"Enter a Number for square: ";
+    cin>>num1;
+    cout<<"Square is "<< num1 * num1 <<endl;
+
+    return 0;
+}
