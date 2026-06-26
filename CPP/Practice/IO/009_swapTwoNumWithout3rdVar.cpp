4766b49d78f6bdf0a8c80de03622085a9afb12fc
shreechandansamal <shreechandansamal@gmail.com>
2026-06-26 12:45:20 +0530

    IO: added 009_swapTwoNumWithout3rdVar.cpp

---
+++ CPP/Practice/IO/009_swapTwoNumWithout3rdVar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    
+    cout<<"Enter Two Numbers for Swap: ";
+    cin>>n1>>n2;
+    cout<<"==Before Swap==\n";
+    cout<<"Number1: "<<n1<< "\nNumber2:= "<<n2<<endl;
+
+    /*method1: Using arithmetic*/
+    // n1 = n1 + n2;
+    // n2 = n1 - n2;
+    // n1 = n1 - n2;
+
+    /*method2: Using XOR*/
+    n1 = n1 ^ n2;
+    n2 = n1 ^ n2;
+    n1 = n1 ^ n2;
+
+    cout<<"==After Swap==\n";
+    cout<<"Number1: "<<n1<< "\nNumber2:= "<<n2<<endl;
+
+    return 0;
+}
