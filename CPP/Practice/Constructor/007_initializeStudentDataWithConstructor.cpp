4fbf9b8c5362a366e99922eb3f31000ab24dd3e0
shreechandansamal <shreechandansamal@gmail.com>
2026-07-06 23:05:12 +0530

    added 007_initializeStudentDataWithConstructor.cpp

---
+++ CPP/Practice/Constructor/007_initializeStudentDataWithConstructor.cpp
@@ -0,0 +1,43 @@
+/**
+ * Task:
+ *  Define a class student and write a program to enter student details
+ *  using constructor and define member function to display all the details.
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Student
+{
+    private:
+        int id;
+        char name[15];
+        int age;
+        int standard;
+    public:
+        Student(int a, const char* b, int c, int d)
+        {
+            id = a;
+            strcpy(name, b);
+            age = c;
+            standard = d;
+        }
+        // void setDetails()
+        void showDetails()
+        {
+            cout << "Id: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+            cout << "Standard: " << standard << endl;
+        }
+};
+
+int main()
+{
+    Student student(1, "Chandan", 24, 10);
+    student.showDetails();
+
+    return 0;
+}
