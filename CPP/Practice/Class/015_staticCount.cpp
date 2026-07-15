b9cf6dcda832e0f2bc300bd720c6226030359863
shreechandansamal <shreechandansamal@gmail.com>
2026-07-15 17:23:29 +0530

    modified 015_staticCount.cpp

--- CPP/Practice/Class/015_staticCount.cpp
+++ CPP/Practice/Class/015_staticCount.cpp
@@ -3,6 +3,12 @@
  *  Define a class StaticCount and create a static variable.
  *  Increment this variable in a function and call this 3 times
  *  and display the result.
+ * 
+ * Learn:
+ *  -how static keyword works.
+ *  -how we can initialize the static properties (using constructor and 
+ *                                                initialize with scope 
+ *                                                scope resolution)
  */
 
 #include <iostream>
@@ -14,16 +20,26 @@ class StaticCount{
         static int count;
 
     public:
-        int getCount() {return count++;}
+        StaticCount() { count = 0; }
+        int incCount() {return count++;}
+        int getCount() {return count;}
 };
 
-int StaticCount::count = 0; //init count here..
+//initialize count here..
+/* int StaticCount::count = 0;    not needed now as we have override
+                                  the default constructor and that 
+                                  will initialize property to 0 */
+int StaticCount::count;
 
 int main()
 {
     StaticCount count;
-    cout<<"count: "<<count.getCount()<<endl;
-    cout<<"count: "<<count.getCount()<<endl;
-    cout<<"count: "<<count.getCount()<<endl;
+    cout<<"Before Increment: "<<count.getCount()<<endl;
+    count.incCount();
+    cout<<"After Increment: "<<count.getCount()<<endl;
+    count.incCount();
+    cout<<"After Increment: "<<count.getCount()<<endl;
+    count.incCount();
+    cout<<"After Increment: "<<count.getCount()<<endl;
     return 0;
 }
